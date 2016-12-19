3bf1580d4879be40c18e1f2f4b9c118c75fa60b2
Irshad <irshad.alam@berylsystems.com>
2016-12-19 19:51:01 +0530

    added IonicJsApp and signupApp

---
+++ IonicFramework/signupViaEmailApp/www/js/Controller.js
@@ -0,0 +1,40 @@
+angular.module('signupApp')
+
+.controller('signupCtrl', function($http){
+  var vm=this;
+  vm.message="hello";
+  vm.userData={
+          "user": {
+            "data": {
+                "type": "users",
+                "attributes": {
+                  "provider": "email",
+                  "device_udid": "E5FB14E9-123A-498A-BBD8-BFF4E1B28059",
+                  "user_progress": "1",
+                  "device_token": "3r2rc34cr3rc43c34t3t3xrv4y56h",
+                    "device_type": "android"
+                }
+            }
+         }
+      }
+
+    
+  
+
+  vm.submitData = function(){
+    $http({
+      method: 'POST',
+      url: 'http://gase.craveit.in/api/users',
+      data: $scope.userData.user.data.type.attributes
+    }).then(function mySuccess(response){
+            vm.myWelcome = response.data;
+            },function myError(response){
+                vm.myWelcome = response.data;
+                });
+
+  
+  };  
+});
+
+
+
---
+++ IonicFramework/signupViaEmailApp/www/js/config.js
@@ -0,0 +1,18 @@
+angular.module('signupApp')
+
+.config(function($stateProvider, $urlRouterProvider){
+  $stateProvider
+  .state('home',{
+    abstract: true,
+    templateUrl: 'templates/home.html'
+    controller: 'homeCtrl'
+  })
+
+  .state('home.signup',{
+  	url:'/signup',
+  	views: {
+      'signup-tab': {
+        templateUrl:'templates/signup.html',
+        controller:'signupCtrl'
+      }
+  });
