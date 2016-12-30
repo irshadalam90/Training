1866cf137922ebb6c9e80f98b4ffc8d6509c163e
Irshad <irshad.alam@berylsystems.com>
2016-12-30 19:41:45 +0530

    added angular toast and date picker

--- IonicFramework/signupViaEmailApp/www/js/Controller.js
+++ IonicFramework/signupViaEmailApp/www/js/Controller.js
@@ -8,7 +8,7 @@ angular.module('signupApp')
 
   })
 
-.controller('signupCtrl', function($scope, $http, $ionicHistory){
+.controller('signupCtrl', function($scope, $http, $ionicHistory, ionicToast, ionicDatePicker){
   //var vm=this;
     $scope.userData={
           "user": {
@@ -26,10 +26,12 @@ angular.module('signupApp')
       }
 
     
-  
+  $scope.myGoBack = function() {
+      $ionicHistory.goBack();
+     }
 
-  $scope.submitData = function(){
-    /*$http({
+  $scope.submitData = function(isValid){
+   /* $http({
       method: 'POST',
       url: 'http://gase.craveit.in/api/users',
       data: $scope.userData.user.data.type.attributes
@@ -37,14 +39,48 @@ angular.module('signupApp')
             $scope.myWelcome = response.data;
             },function myError(response){
                 $scope.myWelcome = response.data;
-                });*/
+                });
+*/
+      if(isValid){
+        ionicToast.show('Account created Successfully', 'middle', false, 2500);
+        //alert('Account created');
+      } 
+      else{
+       ionicToast.show("field can't be empty", 'middle', false, 2500); 
+      }
 
+
+    }
+
+    var ipObj1 = {
+      callback: function (val) {  //Mandatory
+        console.log('Return value from the datepicker popup is : ' + val, new Date(val));
+      },
+      disabledDates: [            //Optional
+        new Date(2016, 2, 16),
+        new Date(2015, 3, 16),
+        new Date(2015, 4, 16),
+        new Date(2015, 5, 16),
+        new Date('Wednesday, August 12, 2015'),
+        new Date("08-16-2016"),
+        new Date(1439676000000)
+      ],
+      from: new Date(2012, 1, 1), //Optional
+      to: new Date(2016, 10, 30), //Optional
+      inputDate: new Date(),      //Optional
+      mondayFirst: true,          //Optional
+      disableWeekdays: [0],       //Optional
+      closeOnSelect: false,       //Optional
+      templateType: 'popup'       //Optional
+    }
+
+    $scope.openDatePicker = function(){
+      ionicDatePicker.openDatePicker(ipObj1);
+    }
+
+    
   
-  };  
-
-  $scope.myGoBack = function() {
-      $ionicHistory.goBack();
-     }
+   
 });
 
 
